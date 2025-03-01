95122e1c612359b61ddf92fdf400aeab3dc6ea35
Dedestem <david@davidnet.net>
2025-03-01 21:27:58 +0100

    Verification page

--- links/verify_email_link/main.js
+++ links/verify_email_link/main.js
@@ -1,6 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    setTimeout(function () {
-      window.close();
-    }, 2000);
-  });
-  
+document.addEventListener('DOMContentLoaded', async function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get("token");
+    const messageDiv = document.getElementById('response-message');
+
+    if (!token) {
+        document.getElementById("verifying").style.display = "none";
+        document.getElementById("error").style.display = "flex";
+        
+        messageDiv.textContent = 'Something went wrong!';
+        messageDiv.style.color = 'red';
+        console.error("Missing token.");
+        return;
+    }
+
+    const response = await fetch('https://auth.davidnet.net/verify_email', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ token })
+    });
+
+    if (response.ok) {
+        document.getElementById("verifying").style.display = "none";
+        document.getElementById("verified").style.display = "flex";
+
+        setTimeout(function () {
+            window.close();
+        }, 5000);
+    } else {
+        document.getElementById("verifying").style.display = "none";
+        document.getElementById("error").style.display = "flex";
+
+        messageDiv.textContent = 'Something went wrong!';
+        messageDiv.style.color = 'red';
+    }
+});
+
