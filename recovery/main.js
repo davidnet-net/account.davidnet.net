26b4747d82e5eca26b310a0a31c587705cfd0d20
Dedestem <david@davidnet.net>
2025-03-02 12:39:59 +0100

    Frontend complete password reset + Recovery framework

---
+++ recovery/main.js
@@ -0,0 +1,108 @@
+document.getElementById("recovery-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    document.getElementById("recovery").style.display = "none";
+    document.getElementById("waitforverify").style.display = "flex";
+
+    // Clear previous errors
+    clearErrors();
+
+    // Get form data
+    const email = document.getElementById("email").value;
+
+    // Prepare the data to send to the server
+    const requestData = {
+        email: email,
+    };
+
+    try {
+        // Make the POST request
+        const response = await fetch("https://auth.davidnet.net/start_recovery", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(requestData),
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            const recovery_ticket = result.recovery_ticket;
+
+            const interval = setInterval(async () => {
+                try {
+                    const response = await fetch("https://auth.davidnet.net/recovery_status", {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({ ticket: recovery_ticket }),
+                    });
+
+                    if (!response.ok) {
+                        window.location = "/recovery/error/?err=Token expired!"
+                        clearInterval(interval);
+                    }
+
+                    // Check if the response body is empty before trying to parse it
+                    const responseBody = await response.text();
+                    if (responseBody) {
+                        const result = JSON.parse(responseBody); // Only parse if there's content
+                        console.log(result);
+
+                        if (result.status == 1) {
+                            console.log("Recovery verified!");
+                            clearInterval(interval);
+
+                            const recovery_token = result.token;
+                            console.log("RECOVERY TOKEN: " + recovery_token);
+                            localStorage.setItem("recovery-token", recovery_token);
+                            document.getElementById("waitforverify").style.display = "none";
+                            document.getElementById("list").style.display = "flex";
+                        }
+                    } else {
+                        console.error("Empty response body");
+                    }
+                } catch (error) {
+                    console.error("Error checking email status:", error);
+                }
+            }, 2500);
+        } else {
+            handleErrors(result.error);
+            document.getElementById("recovery").style.display = "flex";
+            document.getElementById("waitforverify").style.display = "none";
+        }
+    } catch (error) {
+        console.error(error);
+        window.location = "/recovery/error/?err=Network security violation!"
+    }
+});
+
+function handleErrors(error) {
+    console.log("Handling error:", error); // Debugging error
+
+    if (error.includes("Invalid email")) {
+        document.getElementById("email").classList.add("error-input");
+        document.getElementById("email-error").textContent =
+            "Please enter a valid email address.";
+    } else if (error.includes("Not verified")) {
+        window.location = "/recovery/error/?err=Email not verified!"
+    } else {
+        window.location = "/recovery/error/?err=Something unexpected wrent wrong!"
+    }
+}
+
+function clearErrors() {
+    // Clear all error messages and input highlights
+    const errorFields = document.querySelectorAll(".error");
+    errorFields.forEach((field) => field.textContent = "");
+    const errorInputs = document.querySelectorAll(".error-input");
+    errorInputs.forEach((input) => input.classList.remove("error-input"));
+}
+
+import { get_session_information, is_session_valid } from "/session.js";
+document.addEventListener("DOMContentLoaded", async () => {
+    const valid = await is_session_valid();
+    if (valid === true) {
+        window.location.href = "https://account.davidnet.net/account/";
+    }
+});
--- recovery/password/main.js
+++ recovery/password/main.js
@@ -1,23 +1,38 @@
-document.getElementById("resetpassword-form").addEventListener("submit", async (e) => {
-    e.preventDefault();
+import { get_session_information, is_session_valid } from "/session.js";
+document.addEventListener("DOMContentLoaded", async () => {
+    const valid = await is_session_valid();
+    if (valid) {
+        window.location.href = "https://account.davidnet.net/account/";
+    }
 
-    document.getElementById("resetpassword").style.display = "none";
-    document.getElementById("waitforverify").style.display = "flex";
+    const recovery_token = localStorage.getItem("recovery-token");
+
+    if (!recovery_token) {
+        window.location.href = "https://account.davidnet.net/recovery/";
+    }
+});
+
+//localStorage.setItem("recovery-token", null);
+
+document.getElementById("recovery-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
 
     // Clear previous errors
     clearErrors();
 
     // Get form data
-    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
     // Prepare the data to send to the server
+    const recovery_token = localStorage.getItem("recovery-token");
     const requestData = {
-        email: email,
+        password: password,
+        token: recovery_token
     };
 
     try {
         // Make the POST request
-        const response = await fetch("https://auth.davidnet.net/start_recovery", {
+        const response = await fetch("https://auth.davidnet.net/recover_password", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -28,79 +43,13 @@ document.getElementById("resetpassword-form").addEventListener("submit", async (
         const result = await response.json();
 
         if (response.ok) {
-            const recovery_ticket = result.recovery_ticket;
-            //localStorage.setItem("recovery_ticket", result.recovery_ticket);
-
-            const interval = setInterval(async () => {
-                try {
-                    const response = await fetch("https://auth.davidnet.net/recovery_status", {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({ ticket: recovery_ticket }),
-                    });
-
-                    if (!response.ok) {
-                        window.location = "/recovery/error/?err=Token expired!"
-                        clearInterval(interval);
-                    }
-
-                    // Check if the response body is empty before trying to parse it
-                    const responseBody = await response.text();
-                    if (responseBody) {
-                        const result = JSON.parse(responseBody); // Only parse if there's content
-                        console.log(result);
-
-                        if (result.status == 1) {
-                            console.log("Recovery verified!");
-                            clearInterval(interval);
-
-                            const recovery_token = result.token;
-                            console.log("RECOVERY TOKEN: " + recovery_token);
-                        }
-                    } else {
-                        console.error("Empty response body");
-                    }
-                } catch (error) {
-                    console.error("Error checking email status:", error);
-                }
-            }, 2500);
+            document.getElementById("pass-recovery").style.display = "none";
+            document.getElementById("success").style.display = "flex";
         } else {
-            handleErrors(result.error);
-            document.getElementById("resetpassword").style.display = "flex";
-            document.getElementById("waitforverify").style.display = "none";
+            window.location = "/recovery/error/?err=Token expired! Or something wrent wrong."
         }
     } catch (error) {
         console.error(error);
         window.location = "/recovery/error/?err=Network security violation!"
-    }
-});
-
-function handleErrors(error) {
-    console.log("Handling error:", error); // Debugging error
-
-    if (error.includes("Invalid email")) {
-        document.getElementById("email").classList.add("error-input");
-        document.getElementById("email-error").textContent =
-            "Please enter a valid email address.";
-    } else if (error.includes("Not verified")) {
-        window.location = "/recovery/error/?err=Email not verified!"
-    } else {
-        window.location = "/recovery/error/?err=Something unexpected wrent wrong!"
-    }
-}
-
-function clearErrors() {
-    // Clear all error messages and input highlights
-    const errorFields = document.querySelectorAll(".error");
-    errorFields.forEach((field) => field.textContent = "");
-    const errorInputs = document.querySelectorAll(".error-input");
-    errorInputs.forEach((input) => input.classList.remove("error-input"));
-}
-
-import { get_session_information, is_session_valid } from "/session.js";
-document.addEventListener("DOMContentLoaded", async () => {
-    const valid = await is_session_valid();
-    if (valid === true) {
-        window.location.href = "https://account.davidnet.net/account/";
     }
 });
