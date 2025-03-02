c795d0ca4d0a4b6834e0b07c99527e1f0155b90e
Dedestem <david@davidnet.net>
2025-03-02 08:13:28 +0100

    ASYNC

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { is_session_valid, get_session_information } from './session.js';
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    if (is_session_valid() == true) {
+document.addEventListener("DOMContentLoaded", async (event) => {
+    const is_session_valid = await is_session_valid()
+    if ( is_session_valid == true) {
         window.location = "https://account.davidnet.net/account/"
     }
 });
