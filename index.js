25eeaae6cb8af69249da7502f57504d1e7311d31
Dedestem <david@davidnet.net>
2025-03-02 09:24:00 +0100

    Redirect to login if not logged in at /

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     const valid = await is_session_valid();
     if (valid === true) {
         window.location.href = "https://account.davidnet.net/account/";
+    } else {
+        window.location.href = "https://account.davidnet.net/login/"
     }
 });
