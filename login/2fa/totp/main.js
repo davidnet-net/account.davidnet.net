24ac59148df810df7245322f3e425a60347e00fe
Dedestem <david@davidnet.net>
2025-03-03 18:06:31 +0100

    Stage 2/2

--- login/2fa/totp/main.js
+++ login/2fa/totp/main.js
@@ -13,7 +13,7 @@ inputs.forEach(input => {
 
 // Auto-focus and validate once all inputs are filled
 inputs.forEach((input, index) => {
-    input.addEventListener("input", () => {
+    input.addEventListener("input", async () => {
         // Focus the next input if current one is filled
         if (input.value && index < inputs.length - 1) {
             inputs[index + 1].focus();
@@ -23,6 +23,39 @@ inputs.forEach((input, index) => {
         if (Array.from(inputs).every(input => input.value.length === 1)) {
             const enteredString = Array.from(inputs).map(input => input.value).join('');
             console.log("Entered TOTP:", enteredString);
+
+            try {
+                const response = await fetch("https://auth.davidnet.net/verify_totp", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ token: localStorage.getItem("early-login-token"), code: enteredString }),
+                });
+        
+                const result = await response.json();
+                console.log(result);
+        
+                if (!response.ok) {
+                    if (result.error == "Invalid code") {
+                        errmsg.innerText = "Invalid code";
+                    } else {
+                        window.location = "https://account.davidnet.net/login";
+                    }                  
+                } else {
+                    localStorage.setItem("session-token", result.session_token);
+                    console.log("Stored session_token: " + result.session_token);
+    
+                    errmsg.style.color = "green";
+                    errmsg.innerText = "TOTP Verified."
+                    setTimeout(() => {
+                        window.location = "https://account.davidnet.net/account";
+                    }, 1000);
+                }
+            } catch (error) {
+                console.error("Error during code validation:", error);
+                window.location = "https://account.davidnet.net/login";
+            }
         }
     });
 
